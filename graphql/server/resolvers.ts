ec9d87fe2ad47f64b48b24e3ca59c6eb0da6f599
Juan <juan.garcia142@udea.edu.co>
2022-09-04 21:16:10 -0500

    Se elimina carpeta model1

--- graphql/server/resolvers.ts
+++ graphql/server/resolvers.ts
@@ -1,7 +1,6 @@
 import { Resolver } from 'types';
-import { model1Resolvers } from '@graphql/server/model1/resolvers';
 import { productResolvers } from '@graphql/server/product/resolvers';
 
-const globalResolvers: Resolver[] = [model1Resolvers, productResolvers];
+const globalResolvers: Resolver[] = [productResolvers];
 
 export { globalResolvers };
--- graphql/server/types.ts
+++ graphql/server/types.ts
@@ -1,12 +1,11 @@
 import { DocumentNode } from 'graphql';
 import { gql } from 'apollo-server-micro';
-import { model1Types } from '@graphql/server/model1/types';
 import { productTypes } from '@graphql/server/product/types';
 
 const CommonTypes = gql`
   scalar Date
 `;
 
-const globalTypes: DocumentNode[] = [CommonTypes, model1Types, productTypes];
+const globalTypes: DocumentNode[] = [CommonTypes, productTypes];
 
 export { globalTypes };
